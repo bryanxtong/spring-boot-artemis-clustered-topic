2a9c355188cd806a44cd6ba9b3fbcb94e0ba05b9
bryanxtong <bryanxtong@gmail.com>
2024-06-20 11:02:49 +0800

    removed unused items in properties files

--- artemis-topic-consumer/src/main/java/com/sbm/artemis/demo/jms/JmsSubscriber.java
+++ artemis-topic-consumer/src/main/java/com/sbm/artemis/demo/jms/JmsSubscriber.java
@@ -21,18 +21,18 @@ public class JmsSubscriber {
 	@Value("${jms.topic.subscription_name:${random.value}}")
 	String subscriptionName;
 
-	@Value("${spring.artemis.port:unknown}")
+	@Value("${jms.artemis.port:unknown}")
 	String port;
 
 	@JmsListener(destination = "${jms.topic.destination}", containerFactory = "pubSubFactory", subscription = "${jms.topic.subscription_name:${random.value}", id = "${jms.topic.client_id:${random.value}}")
 	public void receive(Company msg){
-		try {
-			Runtime.getRuntime().exec("clear");
+//		try {
+//			Runtime.getRuntime().exec("clear");
 //			System.out.print("\033[H\033[2J");
 //			System.out.flush();
-		} catch (IOException e) {
-			log.error(e.getMessage());
-		}
+//		} catch (IOException e) {
+//			log.error(e.getMessage());
+//		}
 		System.out.println(String.format("=========================CONSUMER %s RUNNING WITH SUBSCRIPTION %s ON NODE @ PORT %s=========================", clientID, subscriptionName, port));
 		System.out.println("Recieved Message: " + msg);
 	}
